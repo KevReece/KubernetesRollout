4bb2913e5070253082c410d1c62352fa9645a125
Kev Reece <kevinjamesreece@gmail.com>
2024-08-27 08:38:06 +0200

    feat: move infra prerequisite check to github action

--- terraform-prerequisites/prerequisites.tf
+++ terraform-prerequisites/prerequisites.tf
@@ -2,14 +2,6 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-data "aws_s3_bucket" "existing_state_bucket" {
-  bucket = "terraform-statefile"
-
-  lifecycle {
-    ignore_changes = all
-  }
-}
-
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-statefile"
 
@@ -21,8 +13,6 @@ resource "aws_s3_bucket" "terraform_state" {
     Name        = "terraform-state"
     Environment = "dev"
   }
-
-  count = length([for b in data.aws_s3_bucket.existing_state_bucket: b.id]) == 0 ? 1 : 0
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
@@ -30,14 +20,6 @@ resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
 
   versioning_configuration {
     status = "Enabled"
-  }
-}
-
-data "aws_dynamodb_table" "existing_lock_table" {
-  name = "terraform-lock"
-
-  lifecycle {
-    ignore_changes = all
   }
 }
 
@@ -55,6 +37,4 @@ resource "aws_dynamodb_table" "terraform_lock" {
     Name        = "terraform-lock"
     Environment = "dev"
   }
-
-  count = length([for t in data.aws_dynamodb_table.existing_lock_table: t.id]) == 0 ? 1 : 0
 }
